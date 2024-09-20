f87a683f1130378fce533153b7ff9977aabf9eb1
TRAORE LACINA <149535272+Traorelacina@users.noreply.github.com>
2024-09-20 09:24:16 +0000

    Add files via upload

---
+++ FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlaneX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assigning the offset value as per the hint
+        offset = new Vector3(30, 0, 10);
+    }
+
+    // Update is called once per frame
+    void LateUpdate() // Using LateUpdate to ensure camera follows after the plane moves
+    {
+        // Make the camera follow the plane's position with the offset
+        transform.position = plane.transform.position + offset;
+    }
+}
+
---
+++ SpinPropellerX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float rotationSpeed = 1000f; // Vitesse de rotation en degrés par seconde
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Faire tourner l'objet autour de l'axe Z
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
